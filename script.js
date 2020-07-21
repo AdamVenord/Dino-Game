8ad165d38dda5305dd608ead758ad204c7f02cab
Adam Venord <AdamVenord7@gmail.com>
2020-07-21 14:46:49 -0600

    speed fix

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function jump(){
     character.classList.add("animate");
     setTimeout(function(){
       character.classList.remove("animate");
-  }, 300);
+  }, 500);
 }
 
 var checkDead = setInterval(function() {
   let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
   let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-  if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+  if(blockLeft<30 && blockLeft>-30 && characterTop>=140){
       block.style.animation = "none";
       alert("Game Over. score: "+Math.floor(counter/100));
       counter=0;
